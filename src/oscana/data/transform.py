17294410a7f338e6611bd135e0940aabbb4721f2
Aditya Marathe <72881014+aditya-marathe@users.noreply.github.com>
2025-10-30 15:47:28 +0000

    Added the `is_cut` and `is_transform` methods to `TransformBase`. Added `prefix`, `name` and `date` attributes to make it easy get information about the transformation.

--- src/oscana/data/transform.py
+++ src/oscana/data/transform.py
@@ -13,12 +13,47 @@ from __future__ import annotations
 
 __all__ = ["TransformBase"]
 
-from abc import ABC, abstractmethod
-
 from typing import TYPE_CHECKING, Any
+
+import re
+from abc import ABC, abstractmethod
 
 if TYPE_CHECKING:
     from .data_handler import DataHandler
+
+
+# ============================== [ Constants  ] ============================== #
+
+TRANSFORM_NAME_REGEX = re.compile(r"^(tfm|cut)_(\d{8})_(.+)$")
+
+
+# =========================== [ Helper Functions ] =========================== #
+
+
+def _get_name_matches(class_name: str) -> re.Match[str]:
+    """\
+    [ Internal ] Get the regex match object for the transform class name.
+
+    Parameters
+    ----------
+    class_name : str
+        The class name of the transform.
+
+    Returns
+    -------
+    re.Match[str]
+        The regex match object for the transform class name.
+    """
+    match = TRANSFORM_NAME_REGEX.match(class_name)
+
+    if match is None:
+        raise ValueError(
+            f"Transform class name `{class_name}` does not conform to the "
+            'naming convention "tfm_<date>_<name>" or "cut_<date>_<name>".'
+        )
+
+    return match
+
 
 # ============================== [ Transforms ] ============================== #
 
@@ -59,6 +94,66 @@ class TransformBase(ABC):
             The data and cuts tables.
         """
         pass
+
+    def is_cut(self) -> bool:
+        """\
+        Check if the transform is a cut.
+
+        Returns
+        -------
+        bool
+            True if the transform is a cut, False otherwise.
+        """
+        return self.prefix == "cut"
+
+    # Note: I can see how this method name can cause some confusion...
+
+    def is_transform(self) -> bool:
+        """\
+        Check if the transform is a transform.
+
+        Returns
+        -------
+        bool
+            True if the transform is a transform, False otherwise.
+        """
+        return self.prefix == "tfm"
+
+    @property
+    def prefix(self) -> str:
+        """\
+        Get the prefix of the transform function.
+
+        Returns
+        -------
+        str
+            The prefix of the transform function.
+        """
+        return _get_name_matches(self.__class__.__name__).group(1)
+
+    @property
+    def name(self) -> str:
+        """\
+        Get the name of the transform function.
+            Returns
+        -------
+        str
+            The name of the transform function.
+        """
+        return _get_name_matches(self.__class__.__name__).group(3)
+
+    @property
+    def date(self) -> str:
+        """\
+        Get the date of the transform function.
+
+        Returns
+        -------
+        datetime
+            The date of the transform function.
+        """
+        date_str = _get_name_matches(self.__class__.__name__).group(2)
+        return f"{date_str[6:8]}/{date_str[4:6]}/{date_str[0:4]}"
 
     def __call__(self, dh: DataHandler) -> tuple[Any, Any]:
         """\
